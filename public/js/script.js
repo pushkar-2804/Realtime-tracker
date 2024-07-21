797d2738334b483e729f6d7f6b8ad2af969201fe
pushkar-28 <kharepushkar2804@gmail.com>
2024-07-22 05:06:46 +0530

    feat: dont open without location access

--- public/js/script.js
+++ public/js/script.js
@@ -18,7 +18,9 @@ navigator.permissions.query({ name: "geolocation" }).then((result) => {
       },
       (error) => {
         // Permission denied or another error occurred
-        alert("Geolocation permission is necessary");
+        alert(
+          "Geolocation permission is necessary. Go to settings and enable it. You cant view other's location without sharing your own"
+        );
         location.reload();
       }
     );
